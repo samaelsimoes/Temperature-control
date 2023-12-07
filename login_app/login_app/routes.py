b9b7ff2a62deb3ecf73b575cbe1943dd0fe49d21
David Zimmermann <davidzimmermannneto@gmail.com>
2023-12-06 22:44:16 -0300

    Criação do Endpoint Análise Preditiva

--- login_app/login_app/routes.py
+++ login_app/login_app/routes.py
@@ -4,6 +4,10 @@ from datetime import datetime, timedelta
 import secrets
 import os
 import csv
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import joblib
 
 users = {
     'admin': 'admin',
@@ -60,6 +64,32 @@ def dashboard_page():
     else:
         return redirect(url_for('login'))
 
+# Função para criar e treinar a rede neural
+def treinar_rede_neural(dados_consumo):
+    # Extrair features e labels
+    features = [float(dado['potencia']) for dado in dados_consumo]
+    labels = [float(dado['totalGasto']) if 'totalGasto' in dado and dado['totalGasto'].replace('.', '', 1).isdigit() else 0 for dado in dados_consumo]
+
+    # Normalizar os dados
+    scaler = MinMaxScaler()
+    features = np.array(features).reshape(-1, 1)
+    labels = np.array(labels).reshape(-1, 1)
+    features = scaler.fit_transform(features)
+    labels = scaler.fit_transform(labels)
+
+    # Construir o modelo da rede neural
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(32, activation='relu', input_shape=(1,)),
+        tf.keras.layers.Dense(1)
+    ])
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    # Treinar o modelo
+    model.fit(features, labels, epochs=50)
+
+    return model, scaler
+
 @app.route('/consumo_chuveiro', methods=['GET'])
 def consumo_chuveiro():
     # Parâmetros da requisição
@@ -88,6 +118,47 @@ def consumo_chuveiro():
         'status': 'success',
         'message': f'Dados de consumo para todos os chuveiros no intervalo de {data_inicio_str} a {data_fim_str}:',
         'consumo_chuveiro': dados_consumo
+    }
+
+    return jsonify(response_data)
+
+@app.route('/analise_preditiva', methods=['GET'])
+def analise_preditiva():
+    # Parâmetros da requisição
+    modelo_analise_preditiva = joblib.load(r"C:\Users\david\Documents\login_page\Temperature-control\modelo_analise_preditiva.joblib")
+    data_inicio_str = request.args.get('data_inicio', '2023-01-01')
+    data_fim_str = request.args.get('data_fim', '2023-12-31')
+
+    # Ler dados do arquivo CSV
+    with open('dados_chuveiros.csv', mode='r') as file:
+        reader = csv.DictReader(file)
+        dados_consumo = [float(row['consumo']) for row in reader]
+
+    # Prever o consumo para o próximo mês
+    predicoes = []
+    for i in range(1):  # Próximo mês
+        # Gerar a data para o próximo mês
+        data_prevista = datetime.strptime(data_fim_str, '%Y-%m-%d') + timedelta(days=1)
+
+        # Use a potência do último registro (supondo que a potência é constante)
+        potencia = float(dados_consumo[-1])
+
+        # Use o modelo treinado para prever o consumo
+        consumo_previsto = modelo_analise_preditiva.predict([[len(dados_consumo) + i]])
+
+        # Adicione as predições à lista
+        predicoes.append({
+            'data': data_prevista.strftime('%Y-%m-%d'),  # Data formatada como string
+            'potencia': str(potencia),  # Potência em string
+            'totalGasto': str(consumo_previsto[0]),  # Valor previsto em string
+            'valor': str(consumo_previsto[0])  # Valor previsto em string
+        })
+
+    # Crie um JSON de resposta
+    response_data = {
+        'status': 'success',
+        'message': 'Dados de consumo previsto para o próximo mês:',
+        'analisePreditiva': predicoes
     }
 
     return jsonify(response_data)
---
+++ login_app/train_model.py
@@ -0,0 +1,31 @@
+import csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+import joblib
+
+# Ler dados do arquivo CSV
+with open('login_app\dados_chuveiros.csv', mode='r') as file:
+    reader = csv.DictReader(file)
+
+    # Extrair dados de consumo
+    dados_consumo = [float(row['consumo']) for row in reader]
+
+# Treinar modelo de análise preditiva
+X = [[i] for i in range(len(dados_consumo))]
+y = dados_consumo
+
+# Dividir dados em conjuntos de treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Padronizar os dados
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Treinar o modelo de análise preditiva
+model = MLPRegressor(random_state=42)
+model.fit(X_train, y_train)
+
+# Salvar o modelo treinado
+joblib.dump(model, 'modelo_analise_preditiva.joblib')
